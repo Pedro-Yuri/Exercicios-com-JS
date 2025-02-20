7e1f0249850a235d9d9b5a157194f8457228898a
Pedro-Yuri <162758122+Pedro-Yuri@users.noreply.github.com>
2025-02-20 17:04:51 -0400

    Add files via upload

---
+++ exercios de js.js/js/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // codição simples
+   console.log('você ULTRPASSOU a velocidade permitida,MULTADO!')   
+}
+console.log('dirija sempre usando cinto de segurança')
+ 
